455771260c3ff6adc99fdf78ebea0c6bd8662ddd
ZAIN ALI <zaincui5542@gmail.com>
2023-12-24 18:19:28 +0500

    commit

--- routes/course.js
+++ routes/course.js
@@ -5,6 +5,7 @@ const courseCtrl = require("../controllers/course");
 router.post("/add", courseCtrl.addCourse);
 router.get("/view", courseCtrl.viewCourses);
 router.get("/update/:id", courseCtrl.updateCourse);
+router.post("/update/:id", courseCtrl.updateCourse);
 router.get("/students/:code", courseCtrl.getStudents);
 router.put("/addStudent/:cid/:sid", courseCtrl.addStudent);
 router.delete("/removeStudent/:cid/:sid", courseCtrl.removeStudent);
